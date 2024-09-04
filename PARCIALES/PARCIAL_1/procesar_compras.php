605364d8ebcccc99ab449158a665d5d4b15cd87d
GABRIEL GARCIA H. <gagarcia@panama-tec.com>
2024-09-03 20:03:19 -0500

    agregando procesar compra, con los calculos

---
+++ PARCIALES/PARCIAL_1/procesar_compras.php
@@ -0,0 +1,35 @@
+<?php
+
+include 'funciones_tienda.php';
+
+$productos = [
+    'camisa' => 50,
+    'pantalon' => 70,
+    'zapatos' => 80,
+    'calcetines' => 10,
+    'gorra' => 25
+];
+
+$carrito = [
+    'camisa' => 2,
+    'pantalon' => 1,
+    'zapatos' => 1,
+    'calcetines' => 3,
+    'gorra' => 0
+];
+
+$subtotal = 0;
+
+foreach ($carrito as $producto => $cantidad) {
+    if ($cantidad > 0) {
+        $subtotal += $productos[$producto] * $cantidad;
+    }
+}
+
+
+$descuento = calcular_descuento($subtotal);
+$impuesto = aplicar_impuesto($subtotal);
+$total = calcular_total($subtotal, $descuento, $impuesto);
+
+
+?>
