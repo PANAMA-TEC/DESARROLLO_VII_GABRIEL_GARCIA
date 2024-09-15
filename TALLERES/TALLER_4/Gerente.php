9943f306a6a2c94a810a2e8d44344090872715f9
GABRIEL GARCIA H. <gagarcia@panama-tec.com>
2024-09-14 23:02:43 -0500

    implementando la clase gerente  2.

--- TALLERES/TALLER_4/Gerente.php
+++ TALLERES/TALLER_4/Gerente.php
@@ -25,8 +25,8 @@
             $this->departamento = $departamento;
         }
 
-        
-    /*
+
+  
         public function evaluar_desempenio() {
             // Lógica para evaluar el desempeño de un gerente
             return "El desempeño del gerente " . $this->nombre . " ha sido evaluado.";
@@ -35,7 +35,7 @@
         public function calcular_salario_total() {
             return $this->salario_base + $this->bono;
         }
-    */
+    
 
 
     }
